fee6dc1d28becfb99c3d008d43a0266320cf48f6
Marquestt <thatyanecmarques98@live.com>
2024-01-24 19:47:20 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+alert("Boas vindas ao jogo do número secreto!");
+let numeroSecreto = 4;
+console.log(numeroSecreto);
+let chute = prompt("Escolha um número entre 1 e 10");
+ 
+//se o chute for = ao número secreto
+if (numeroSecreto == chute){
+    alert("Isso ai, você descobriu o número secreto!");
+}
+//senão
+else {
+    alert("Você errou =(");
+}
